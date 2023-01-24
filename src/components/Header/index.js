bf55eaba327bd59656ae7ad9148f23fe26efb255
IRizzon <italomarquesrizzon@gmail.com>
2023-01-24 12:35:48 -0300

    Primeiro Commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+
+import { useContext } from 'react';
+import './header.css';
+import { AuthContext } from '../../contexts/auth';
+import avatar from '../../assets/avatar.png'
+import { Link } from 'react-router-dom';
+import { FiHome, FiUser, FiSettings } from 'react-icons/fi'
+
+export default function Header(){
+    const { user } = useContext(AuthContext);
+
+    return(
+        <div className='headerbar'>
+            <div>
+                <img src={user.avatarUrl === null ? avatar : user.avatarUrl } alt='Foto avatar'/>
+            </div>
+
+            <Link to='/dashboard'>
+                <FiHome color='#BDECB6' size={24} />
+                Home
+            </Link>
+            <Link to='/dashboard'>
+                <FiUser color='#BDECB6' size={24} />
+                Perfil
+            </Link>
+            <Link to='/dashboard'>
+                <FiSettings color='#BDECB6' size={24} />
+                Configuração
+            </Link>
+        </div>
+    )
+}
---
+++ src/services/fbConect.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyCHRd5Fy4qXWSwjWlZ0eMub3OaUXcgCmuw",
+  authDomain: "reactjstraining-e1f41.firebaseapp.com",
+  projectId: "reactjstraining-e1f41",
+  storageBucket: "reactjstraining-e1f41.appspot.com",
+  messagingSenderId: "939675977766",
+  appId: "1:939675977766:web:649650f957d20101897ee7",
+  measurementId: "G-MRLFDFJL12"
+};
+
+if(!firebase.apps.length){
+firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
